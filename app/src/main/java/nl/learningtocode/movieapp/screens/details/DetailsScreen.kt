6af27e655f5c5b3d6016eb8cf0eeedf39740cd33
jokelugtigheid <jokevtz@gmail.com>
2022-01-11 19:38:06 +0100

    ICON : added app icon

--- app/src/main/java/nl/learningtocode/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/nl/learningtocode/movieapp/screens/details/DetailsScreen.kt
@@ -1,10 +1,49 @@
 package nl.learningtocode.movieapp.screens.details
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun DetailsScreen(navController: NavController, movieData: String?){
-    Text(text = movieData.toString())
+
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.Blue,
+            elevation = 5.dp) {
+            Row() {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Arrow Back Home",
+                modifier = Modifier.clickable { navController.popBackStack() 
+                })
+                Spacer(modifier = Modifier.width(100.dp))
+
+            Text(text = "Marvel Movies")
+            }
+        }
+    }) {
+        Surface(modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            )
+        {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            )
+            {
+            Text(text = movieData.toString(),
+            style = MaterialTheme.typography.h4)
+
+            }
+        }
+    }
+
+
 }
